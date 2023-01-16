15526105f4e8e51b9a80d094eff82833f1875804
Falk Hoppe <falkhoppe81@gmail.com>
2023-01-16 23:29:24 +0100

    improve test benchmark setup

--- bench.js
+++ bench.js
@@ -8,36 +8,42 @@ class MyCustomElement extends HTMLElement {
 
 
     buttons.forEach(b => b.addEventListener('click', (e) => {
+      console.log('marks cleared?:', performance.getEntriesByType("mark").length);
       const memoized = e.target.classList.contains('memoized')
       console.log(`start test run (access ${memoized ? "IS NOT": "IS"} memoized)`);
+
+      const hasAttr  = (memoized ? this.memoizedAttrAccess : this.unmemoizedAttrAccess).bind(this)
 
       for (let i = 0; i < TEST_CYCLES; i++) {
         performance.mark('start')
 
-        const hasAttr = memoized ? this.memoizedAttrAccess : this.unmemoizedAttrAccess
+        const attr = hasAttr()
 
         performance.mark('end')
-        performance.measure('Measurement', {start: 'start', end: 'end', detail: { testVal: hasAttr }})
+        performance.measure('Measurement', { start: 'start', end: 'end', detail: { testVal: attr } })
       }
       console.log('end test cycle');
       console.log('compute results:');
+
       const measurements = performance.getEntriesByName('Measurement')
       const meantime = measurements.reduce((res, curr) => {
         res += curr.duration
         return res
       }, 0) / TEST_CYCLES
-      console.log('results:', 'meantime -> ', meantime);
+      console.log('results:', 'meantime -> ', meantime, ' (for ', performance.getEntriesByName("Measurement").length,' measurements)');
 
-      this.insertAdjacentHTML('afterend', `<p>Meantime for "${memoized ? "memoized" : "not memoized"}" is: ${meantime}</p>`)
+      this.insertAdjacentHTML('beforeend', `<p>Meantime for "${memoized ? "memoized" : "not memoized"}" is: ${meantime}</p>`)
+
       performance.clearMarks()
+      performance.clearMeasures()
     }))
   }
 
-  get unmemoizedAttrAccess() {
+  unmemoizedAttrAccess() {
     return this.innerElement.hasAttribute('test-attr')
   }
 
-  get memoizedAttrAccess() {
+  memoizedAttrAccess() {
     if (this.#innerTestAttribute) return this.#innerTestAttribute
 
     this.#innerTestAttribute = this.innerElement.hasAttribute('test-attr')
